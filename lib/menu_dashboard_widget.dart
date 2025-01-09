9cbd56a2cf83074ddb292860fe561d414d424b7b
suleymansurucu <suleymansurucu95@gmail.com>
2025-01-09 10:39:19 -0500

    feet : Scale Animation, Tween

--- lib/menu_dashboard_widget.dart
+++ lib/menu_dashboard_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+// Theme and Styles
 final TextStyle menuFontStyle = TextStyle(color: Colors.white, fontSize: 20);
 final Color backgroundColor = Color(0xFF343442);
 
@@ -12,48 +13,52 @@ class MenuDashboardWidget extends StatefulWidget {
 
 class _MenuDashboardWidgetState extends State<MenuDashboardWidget>
     with SingleTickerProviderStateMixin {
-  late double screenHeight, screeWidth;
+  late double screenHeight, screenWidth;
   bool isMenuOpen = false;
 
   late AnimationController _controller;
   late Animation<double> _scaleAnimation;
-  final Duration _duration = Duration(milliseconds: 750);
-   late Animation<Offset> _menuOffsetAnimation;
+  late Animation<Offset> _menuOffsetAnimation;
+  final Duration _duration = Duration(milliseconds: 500);
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     _controller = AnimationController(vsync: this, duration: _duration);
-    _scaleAnimation = Tween(begin: 1.0, end: 0.6).animate(_controller);
-    _menuOffsetAnimation=Tween(begin: Offset(-1, 0), end: Offset(0, 0)).animate(CurvedAnimation(parent: _controller, curve: Curves.easeIn));
+    _scaleAnimation = Tween(begin: 1.0, end: 0.6).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
+    );
+    _menuOffsetAnimation = Tween(begin: Offset(-1, 0), end: Offset(0, 0)).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
+    );
   }
 
   @override
   void dispose() {
     _controller.dispose();
-    // TODO: implement dispose
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     screenHeight = MediaQuery.of(context).size.height;
-    screeWidth = MediaQuery.of(context).size.height;
+    screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
       backgroundColor: backgroundColor,
       body: SafeArea(
         child: Stack(
           children: [
-            buildMenu(context),
-            buildDashboard(context),
+            buildMenu(),
+            buildDashboard(),
           ],
         ),
       ),
     );
   }
 
-  buildMenu(BuildContext context) {
+  Widget buildMenu() {
+    final menuItems = ["Dashboard", "Message", "Utility Bills", "Fund Transfer", "Branches"];
     return SlideTransition(
       position: _menuOffsetAnimation,
       child: Padding(
@@ -62,135 +67,97 @@ class _MenuDashboardWidgetState extends State<MenuDashboardWidget>
           alignment: Alignment.centerLeft,
           child: Column(
             mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
             crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Dashboard',
-                style: menuFontStyle,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Text(
-                'Message',
-                style: menuFontStyle,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Text(
-                'Utility Bills',
-                style: menuFontStyle,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Text(
-                'Fund Transfer',
-                style: menuFontStyle,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Text(
-                'Branches',
-                style: menuFontStyle,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Text(
-                'Dashboard',
-                style: menuFontStyle,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-            ],
+            children: menuItems
+                .map((item) => Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: Text(item, style: menuFontStyle),
+            ))
+                .toList(),
           ),
         ),
       ),
     );
   }
 
-  buildDashboard(BuildContext context) {
+  Widget buildDashboard() {
     return AnimatedPositioned(
-      top: isMenuOpen ? 0.02 * screenHeight : 0,
-       bottom: isMenuOpen ? 0.02 * screenHeight : 0,
-      left: isMenuOpen ? 0.18 * screeWidth : 0,
       duration: _duration,
+      top: isMenuOpen ? 0.05 * screenHeight : 0,
+      bottom: isMenuOpen ? 0.05 * screenHeight : 0,
+      left: isMenuOpen ? 0.2 * screenWidth : 0,
+      right: isMenuOpen ? -0.2 * screenWidth : 0,
       child: ScaleTransition(
         scale: _scaleAnimation,
         child: Material(
-          borderRadius: isMenuOpen ? BorderRadius.all(Radius.circular(40)) : null,
+          borderRadius: isMenuOpen ? BorderRadius.circular(40) : BorderRadius.zero,
           elevation: 8,
           color: backgroundColor,
           child: Container(
-            width: MediaQuery.of(context).size.width,
-            padding: EdgeInsets.only(left: 16, right: 16, top: 8),
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
             child: Column(
               children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    InkWell(
-                      onTap: () {
-                        setState(() {
-                          if (isMenuOpen) {
-                            _controller.reverse();
-                          } else{
-                            _controller.forward();
-                          }
-                          isMenuOpen = !isMenuOpen;
-
-                        });
-                      },
-                      child: Icon(
-                        Icons.menu,
-                        color: Colors.white,
-                      ),
-                    ),
-                    Text(
-                      'My Card',
-                      style: TextStyle(color: Colors.white, fontSize: 24),
-                    ),
-                    Icon(
-                      Icons.add_circle,
-                      color: Colors.white,
-                    ),
-                  ],
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: 20),
-                  height: 200,
-                  child: PageView(
-                    scrollDirection: Axis.horizontal,
-                    children: [
-                      Container(
-                        color: Colors.pink,
-                        width: 100,
-                        margin: EdgeInsets.symmetric(horizontal: 12),
-                        child: Center(child: Text("Container" , style: TextStyle(fontSize: 24),)),
-                      ),
-                      Container(
-                        color: Colors.purple,
-                        width: 100,
-                        margin: EdgeInsets.symmetric(horizontal: 12),
-                      ),
-                      Container(
-                        color: Colors.tealAccent,
-                        width: 100,
-                        margin: EdgeInsets.symmetric(horizontal: 12),
-                      ),
-                    ],
-                  ),
-                )
+                buildHeader(),
+                SizedBox(height: 20),
+                buildPageView(),
               ],
             ),
           ),
         ),
       ),
     );
+  }
+
+  Widget buildHeader() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        InkWell(
+          onTap: toggleMenu,
+          child: Icon(Icons.menu, color: Colors.white),
+        ),
+        Text(
+          'My Card',
+          style: TextStyle(color: Colors.white, fontSize: 24),
+        ),
+        Icon(Icons.add_circle, color: Colors.white),
+      ],
+    );
+  }
+
+  Widget buildPageView() {
+    final cardColors = [Colors.pink, Colors.purple, Colors.tealAccent];
+    return SizedBox(
+      height: 200,
+      child: PageView(
+        scrollDirection: Axis.horizontal,
+        children: cardColors
+            .map((color) => Container(
+          margin: const EdgeInsets.symmetric(horizontal: 12),
+          decoration: BoxDecoration(
+            color: color,
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: Center(
+            child: Text(
+              "Container",
+              style: TextStyle(fontSize: 24, color: Colors.white),
+            ),
+          ),
+        ))
+            .toList(),
+      ),
+    );
+  }
+
+  void toggleMenu() {
+    setState(() {
+      if (isMenuOpen) {
+        _controller.reverse();
+      } else {
+        _controller.forward();
+      }
+      isMenuOpen = !isMenuOpen;
+    });
   }
 }
